d68ca3cd70aca19fae44bd79f8470ee3ada58588
diegoidef <diego0211@gmail.com>
2019-11-20 17:51:58 +0100

    'Theme'

---
+++ idef/lang/en/theme_idef.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package   theme_idef
+ * @copyright 2019 by IDEF
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+// Raw SCSS setting.
+$string['choosereadme'] = 'Welcome to IDEF theme :). A customisable moodle theme, based on Boost, with an alternative navigation layout. Please, respect anyway moodle core!!';
+$string['configtitle'] = 'idef';
+$string['navbardark'] = 'Use a dark style navbar';
+$string['navbardarkdesc'] = 'Swaps text and background colours for the navbar at the top of the page between dark and light.';
+$string['pluginname'] = 'IDEF 21 :)';
+$string['presetfiles'] = 'Additional theme preset files';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href="https://docs.moodle.org/dev/Boost_Presets">Boost presets</a> for information on creating and sharing your own preset files, and see the <a href="https://archive.moodle.net/boost">Presets repository</a> for presets that others have shared.';
+$string['preset'] = 'Theme preset';
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['sitetitle'] = 'Site title';
+$string['privacy:metadata'] = 'The IDEF theme does not store any personal data.';
+$string['new_categoryd'] = 'Please choose the category id for new courses. Please, contact with your academic Organization';
+$string['new_category'] = 'New courses category (id number)';
+$string['new_courses_info'] = 'Please, notice the new Courses!! To enroll them, contact with Learning Department :)';
+$string['enddate_info'] = 'Please, remember this course will become disabled at ';
---
+++ idef/readme.txt
@@ -0,0 +1,16 @@
+/**
+ * idef theme
+ *
+ * @package   theme_idef
+ * @copyright 2019 by IDEF
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+This theme overriddes core funtions and plugins fuctions.
+Please, in any case respect Moodle core :)
+The theme_idef requires at least Moodle version 2019051100.
+It needs theme_boost and format_topcoll
+In theme_idef config we can select the category id to show in frontpage new course to allow students enrollment
+
+ 
